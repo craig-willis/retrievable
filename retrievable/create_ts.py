bef9f3629686ada76bea5a51fd33812ee617a2d7
Craig Willis <willis8@illinois.edu>
2018-11-30 18:57:30 +0000

    Added logging/verbose flag

--- retrievable/create_ts.py
+++ retrievable/create_ts.py
@@ -1,7 +1,9 @@
 import argparse
-import time
+import logging
 import pyndri
 import pandas as pd
+import time
+
 from datetime import datetime
 from tqdm import tqdm
 
@@ -10,19 +12,27 @@ def main(args=None):
 
     parser = argparse.ArgumentParser(
             description='Create term timeseries index')
-    parser.add_argument('--index', dest='index')
-    parser.add_argument('--output', dest='output')
+    parser.add_argument('-i', '--index', dest='index', help='Input index path')
+    parser.add_argument('-o', '--output', dest='output', help='Output path')
+    parser.add_argument("-v", "--verbose", help='Verbose logging', 
+                    action="store_true")
+
 
     args = parser.parse_args()
 
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
     index = pyndri.Index(args.index)
 
-    print('Reading dictionary...')
+    logging.info('Get dictionary')
     token2id, id2token, id2df = index.get_dictionary()
 
     doc_ids = range(index.document_base(), index.maximum_document())
 
-    print('Building timeseries...')
+    logging.info('Building index')
     ts = {}
     for doc_id in tqdm(doc_ids):
 
@@ -41,17 +51,17 @@ def main(args=None):
 
                 ts[date][token_id] += 1
 
+    logging.info('Creating dataframe')
     t0 = time.time()
-    print('Creating dataframe...')
     df = pd.DataFrame.from_dict(ts, orient='index', dtype=int)
     t1 = time.time()
-    print(t1 - t0)
+    logging.debug("time: %s" % (t1 - t0))
 
+    logging.info('Serializing dataframe')
     t0 = time.time()
-    print('Serializing dataframe...')
     df.to_csv(args.output, compression="gzip")
     t1 = time.time()
-    print(t1 - t0)
+    logging.debug("time: %s" % (t1 - t0))
 
 
 if __name__ == "__main__":
